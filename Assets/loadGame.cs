36a2d1567464f2ff177b356a9961f328467f0260
joshuatwomey <18002078@student.uws.edu.au>
2014-10-30 19:46:38 +1100

    ContraRoll is a FlappyBird clone, with Billy the NES Contra character rolling through mountains.

---
+++ Assets/loadGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadGame : MonoBehaviour {
+	void Update () 
+	{
+		if (Input.GetMouseButton (0)) 
+		{
+			Application.LoadLevel ("play");
+		}
+	}
+}
